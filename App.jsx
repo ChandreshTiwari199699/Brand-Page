bc14871a053495713f5a903907d9400797d2663f
ChandreshTiwari199699 <155742283+ChandreshTiwari199699@users.noreply.github.com>
2024-06-18 17:47:18 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+ import './App.css';
+import HeroSection from './component/Hero';
+ import Navigation from './component/Navigation';
+ const App = () => {
+    return <div> 
+        <Navigation/>
+        <HeroSection/>
+    </div>
+};
+export default App;
